57aae792dcf67f4fcf6dd39f31ca31b781e4f4f7
York Cook <yorkcook@Johnny-Castle.lan1>
2019-06-03 18:00:44 -0400

    WIP: class App set up

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // dummyData: dummyData
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Welcome to your Insta App!</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
