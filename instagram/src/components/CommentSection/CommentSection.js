f720310b3ca3c3d9ac1d9bb39d18ce49b2ba3b85
York Cook <yorkcook@Johnny-Castle.lan1>
2019-06-04 11:21:37 -0400

    WIP: buidling out CommentSection

---
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CommentSection = ({ username, text }) => {
+  return (
+    <div className="comment-container">
+      <div className="comment-list">
+        {comments.map(() => {
+          {
+            username;
+          }
+          {
+            text;
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CommentSection;
